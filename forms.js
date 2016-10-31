4edf2c6c734680a237bfa8a4280a4047556f172c
Steve Larsen <stevelarsen01@gmail.com>
2016-10-31 13:45:59 -0500

    add delete functionality

--- forms.js
+++ forms.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     var array = [];
+    var empArray = [];
+    var totalSalary = 0;
     $('#employeeinfo').on('submit', function(event) {
       event.preventDefault();
 
@@ -11,26 +13,38 @@ $(document).ready(function() {
 
       // iterate over the array and transfer each index into a new property on an object with the value of what was entered.
       fields.forEach(function(element, index, array) {
-        // review index notation vs. dot notation on objects
-        // here, dot notation wouldn't work
         values[element.name] = element.value;
       });
 
+      empArray.push(values);
       console.log(values);
 
+      for (var i = 0; i < empArray.length; i++) {
+        totalSalary += empArray[i].employeeSalary;
+      }
+      var monthlySalary = totalSalary / 12;
+      $('#monthlySalary').text(monthlySalary);
+      console.log(monthlySalary);
       // clear out inputs
-      $('#employeeinfo').find('input[type=text]').val('');
+      $('#employeeinfo').find('input[type=text] input[type=number]').val('');
 
       // append to DOM
       appendDom(values);
     });
 
     function appendDom(empInfo) {
-      $('#container').append('<div class="person"></div>');
-      var $el = $('#container').children().last();
-
-      $el.append('<p>' + empInfo.employeefirstname + ' ' + empInfo.employeelastname + '</p>');
+      $('#empBody').append('<tr class="person"><td>' +
+      empInfo.employeeFirstName + '</td><td>' +
+      empInfo.employeeLastName + '</td><td>' +
+      empInfo.employeeID + '</td><td>' +
+      empInfo.employeeTitle + '</td><td>' +
+      empInfo.employeeSalary + '</td>' +
+      '<td><button class="deleteButton">Delete</button></td>' +
+      '</tr>');
     }
 
-
+    $('#container').on('click', '.deleteButton', function(event) {
+      event.preventDefault();
+      $(this).parent().parent().remove();
+    });
 });
